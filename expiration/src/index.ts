a38250d15f305366870322fae1aaae96f2836cb6
muhammedfayiscc <mohammed.fayis@pegasusone.com>
2024-10-08 21:45:11 +0530

    added gracefulshutdown in expiration

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,8 +1,23 @@
+import { KafkaConsumer } from "@fayisorg/common-modules";
 import { startKafkaConsumer } from "./helper";
 
 (async () => {
     try {
-   
+      const shutdown = async () => {
+        try {
+            const kafkaConsumer = new KafkaConsumer();
+            await kafkaConsumer.disconnectConsumer();
+            console.log('KafkaConsumer has been disconnected.');
+            process.exit(0);
+        } catch (error) {
+            console.error('Error during shutdown:', error);
+            process.exit(1);
+        }
+    };
+    
+    // Listen for termination signals
+    process.on('SIGINT', shutdown);
+    process.on('SIGTERM', shutdown);
       await startKafkaConsumer()
 
     } catch (err) {
